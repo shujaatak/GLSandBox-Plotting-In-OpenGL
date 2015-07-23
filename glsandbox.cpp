f718d928a200dbbcba1ead4698ec0d4146aeb529
Dante <dante.a.sanchez@gmail.com>
2015-07-22 21:54:39 -0400

    Blending test

--- glsandbox.cpp
+++ glsandbox.cpp
@@ -12,33 +12,14 @@ GLSandBox::GLSandBox(QWidget *parent)
     if (m_transparent)
         setAttribute(Qt::WA_TranslucentBackground);
 
-    m_blue = 0;
+    m_blue = 200;
+
     triangleCoords.append(QVector2D(60.0f,  10.0f));
     triangleCoords.append(QVector2D(110.0f, 110.0f));
     triangleCoords.append(QVector2D(10.0f,  110.0f));
 
     timer = new QTimer;
     connect(timer,SIGNAL(timeout()),this,SLOT(changeColor()));
-//    timer->start(5);
-
-    // From SuperBible:
-//    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
-//        glutInitWindowSize(800, 600);
-//        glutCreateWindow("Triangle");
-//        glutReshapeFunc(ChangeSize);
-//        glutDisplayFunc(RenderScene);
-
-//        GLenum err = glewInit();
-//        if (GLEW_OK != err) {
-//            fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
-//            return 1;
-//            }
-
-//        SetupRC();
-
-//        glutMainLoop();
-
-
 }
 
 GLSandBox::~GLSandBox()
@@ -66,96 +47,57 @@ void GLSandBox::cleanup()
 
 void GLSandBox::initializeGL()
 {
-    // In this example the widget's corresponding top-level window can change
-    // several times during the widget's lifetime. Whenever this happens, the
-    // QOpenGLWidget's associated context is destroyed and a new one is created.
-    // Therefore we have to be prepared to clean up the resources on the
-    // aboutToBeDestroyed() signal, instead of the destructor. The emission of
-    // the signal will be followed by an invocation of initializeGL() where we
-    // can recreate all resources.
     connect(context(), &QOpenGLContext::aboutToBeDestroyed, this, &GLSandBox::cleanup);
 
     initializeOpenGLFunctions();
 
-    // SetupRC
-    glClearColor(0, 0, 0, m_transparent ? 0 : 1);
-
+    // Blue background
+    glClearColor(0.1f, 0.0f, 0.0f, 1.0f );
     m_program = new QOpenGLShaderProgram;
-    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSimple);
-    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment,
-        "uniform mediump vec4 color;\n"
-        "void main(void)\n"
-        "{\n"
-        "   gl_FragColor = color;\n"
-        "}");
+    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex,vertexShaderSimple);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment,fragmentShaderSimple);
     m_program->link();
     m_program->bind();
-
     vertexLocation = m_program->attributeLocation("vertex");
     matrixLocation = m_program->uniformLocation("matrix");
     colorLocation = m_program->uniformLocation("color");
-
-//    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, m_core ? vertexShaderSourceCore : vertexShaderSource);
-//    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, m_core ? fragmentShaderSourceCore : fragmentShaderSource);
-//    m_program->bindAttributeLocation("vertex", 0);
-//    m_program->bindAttributeLocation("normal", 1);
-//    m_program->link();
-//    m_program->bind();
-
-//    m_camera.setToIdentity();
-//    m_camera.translate(0, 0, -1);
-
-    // Light position is fixed.
-
-//    qDebug() << "OpenGL Version: " << qPrintable(*glGetString(GL_VERSION));
-   }
+}
 
 void GLSandBox::paintGL()
 {
-    QVector2D triangleVerts[] =
-    {
-     QVector2D(60.0f,  10.0f),
-     triangleCoords.at(1),
-     QVector2D(10.0f,  110.0f),
-    };
+    // Clear the window with current clearing color
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    QColor color(50, 50, m_blue, 255);
+    QColor color(0, 50, 200, 255);
 
     QMatrix4x4 pmvMatrix;
     pmvMatrix.ortho(rect());
 
-    m_program->setAttributeArray(vertexLocation, triangleVerts);
     m_program->enableAttributeArray(vertexLocation);
+    m_program->setAttributeArray(vertexLocation, triangleVertices, 3);
     m_program->setUniformValue(matrixLocation, pmvMatrix);
     m_program->setUniformValue(colorLocation, color);
 
+    glLineWidth(2.5f);
     glDrawArrays(GL_LINE_LOOP, 0, 3);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    m_program->setAttributeArray(vertexLocation, squareVertices,3);
+    color = QColor(50,100,0,100);
+    m_program->setUniformValue(colorLocation, color);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+    glDisable(GL_BLEND);
 
     m_program->disableAttributeArray(vertexLocation);
 }
 
 void GLSandBox::resizeGL(int w, int h)
 {
-
-//    m_proj.setToIdentity();
-//    m_proj.perspective(45.0f, GLfloat(w) / h, 0.01f, 100.0f);
+    //    m_proj.setToIdentity();
+    //    m_proj.perspective(45.0f, GLfloat(w) / h, 0.01f, 100.0f);
 }
 
 void GLSandBox::changeColor()
 {
-    m_blue +=10;
-    if(m_blue>=255)
-        m_blue=0;
-
-    int xcoord = triangleCoords.at(1).x();
-    xcoord+=1;
-    if(xcoord>=200)
-        xcoord=1;
-    QVector2D temp = triangleCoords.at(1);
-    temp.setX(xcoord);
-    triangleCoords[1] = temp;
-
-    update();
 }
--- glsandbox.h
+++ glsandbox.h
@@ -10,6 +10,21 @@
 
 #include <QTimer>
 
+static GLfloat const triangleVertices[] = {
+    60.0f,  10.0f,  0.0f,
+    110.0f, 110.0f, 0.0f,
+    10.0f,  110.0f, 0.0f
+};
+
+static GLfloat const squareVertices[] = {
+    80.0f, 100.0f, 0.0f,
+    130.0f,  100.0f, 0.0f,
+    130.0f, 10.0f, 0.0f,
+    80.0f,  10.0f,  0.0f,
+};
+
+
+// Shader sources
 static const char *vertexShaderSimple =
         "attribute highp vec4 vertex;\n"
             "uniform highp mat4 matrix;\n"
@@ -17,61 +32,12 @@ static const char *vertexShaderSimple =
             "{\n"
             "   gl_Position = matrix * vertex;\n"
             "}";
-
-static const char *vertexShaderSourceCore =
-    "#version 150\n"
-    "in vec4 vertex;\n"
-    "in vec3 normal;\n"
-    "out vec3 vert;\n"
-    "out vec3 vertNormal;\n"
-    "uniform mat4 projMatrix;\n"
-    "uniform mat4 mvMatrix;\n"
-    "uniform mat3 normalMatrix;\n"
-    "void main() {\n"
-    "   vert = vertex.xyz;\n"
-    "   vertNormal = normalMatrix * normal;\n"
-    "   gl_Position = projMatrix * mvMatrix * vertex;\n"
-    "}\n";
-
-static const char *fragmentShaderSourceCore =
-    "#version 150\n"
-    "in highp vec3 vert;\n"
-    "in highp vec3 vertNormal;\n"
-    "out highp vec4 fragColor;\n"
-    "uniform highp vec3 lightPos;\n"
-    "void main() {\n"
-    "   highp vec3 L = normalize(lightPos - vert);\n"
-    "   highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
-    "   highp vec3 color = vec3(0.39, 1.0, 0.0);\n"
-    "   highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n"
-    "   fragColor = vec4(col, 1.0);\n"
-    "}\n";
-
-static const char *vertexShaderSource =
-    "attribute vec4 vertex;\n"
-    "attribute vec3 normal;\n"
-    "varying vec3 vert;\n"
-    "varying vec3 vertNormal;\n"
-    "uniform mat4 projMatrix;\n"
-    "uniform mat4 mvMatrix;\n"
-    "uniform mat3 normalMatrix;\n"
-    "void main() {\n"
-    "   vert = vertex.xyz;\n"
-    "   vertNormal = normalMatrix * normal;\n"
-    "   gl_Position = projMatrix * mvMatrix * vertex;\n"
-    "}\n";
-
-static const char *fragmentShaderSource =
-    "varying highp vec3 vert;\n"
-    "varying highp vec3 vertNormal;\n"
-    "uniform highp vec3 lightPos;\n"
-    "void main() {\n"
-    "   highp vec3 L = normalize(lightPos - vert);\n"
-    "   highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
-    "   highp vec3 color = vec3(0.39, 1.0, 0.0);\n"
-    "   highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);\n"
-    "   gl_FragColor = vec4(col, 1.0);\n"
-    "}\n";
+static const char *fragmentShaderSimple =
+    "uniform mediump vec4 color;\n"
+    "void main(void)\n"
+    "{\n"
+    "   gl_FragColor = color;\n"
+    "}";
 
 QT_FORWARD_DECLARE_CLASS(QOpenGLShaderProgram)
 
@@ -113,6 +79,7 @@ private:
 //    QVector2D triangleCoords[3];
 
     QTimer *timer;
+
 };
 
 #endif // GLSANDBOX_H
